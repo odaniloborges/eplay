98122398b9b84fb22a561508fada0d8c7aacec17
Danilo Borges <daniloborges.dev@gmail.com>
2026-02-10 13:05:49 -0300

    finalização

---
+++ src/styles/variaveis.ts
@@ -0,0 +1,10 @@
+export default {
+  vermelho: '#C23616',
+  verde: '#44BD32',
+  amarelo: '#F0C11A',
+  amarelo2: '#E1A32A',
+  azulEscuro: '#2f3640',
+  cinza: '#333',
+  branca: '#eee',
+  preta: '#111'
+}
